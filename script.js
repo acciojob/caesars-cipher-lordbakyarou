7a74d99db44fde3963b0235ec52a3c1787d8778a
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-09 01:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function rot13(encodedStr) {
 for (let i = 0; i  < encodedStr.length; i++) {
     decodedArr[i] = lookup.str[i];
   }
-  return; //return decodedArr
+  return decodedArr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
