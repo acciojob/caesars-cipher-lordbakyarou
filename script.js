3fe32de05ed6e0e9a2c7a539345d088bcae436dd
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-09 01:18:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,7 +34,9 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
+for (let i = 0; i  < encodedStr.length; i++) {
+    decodedArr[i] = lookup.str[i];
+  }
   return; //return decodedArr
 }
 
